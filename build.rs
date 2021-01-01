a0af8e98f0c146d3b3e4ac549720b667fafb6fef
sharkdp <davidpeter@web.de>
2021-01-01 13:37:26 +0100

    Fix clippy suggestion

--- build.rs
+++ build.rs
@@ -1,10 +1,10 @@
 fn main() {
     println!(
         "cargo:rustc-env=BUGREPORT_TARGET={}",
-        std::env::var("TARGET").unwrap_or("Unknown".into())
+        std::env::var("TARGET").unwrap_or_else(|_| "Unknown".into())
     );
     println!(
         "cargo:rustc-env=BUGREPORT_PROFILE={}",
-        std::env::var("PROFILE").unwrap_or("Unknown".into())
+        std::env::var("PROFILE").unwrap_or_else(|_| "Unknown".into())
     );
 }
